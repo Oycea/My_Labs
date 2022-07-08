f53d7f9c667075cced9c1ee3d0afeb89ce6fa968
Маргаритка <95122842+Oycea@users.noreply.github.com>
2022-07-09 00:53:51 +0300

    Update marsh.h

--- Lab_11/marsh.h
+++ Lab_11/marsh.h
@@ -5,38 +5,42 @@
 #include<string>
 using namespace std;
 
-class marsh{
+class marsh {
 private:
-	// Начальный и конечный пункты маршрута.
-	string _start_point, _end_point;
 	// Номер маршрута.
 	int _route_number;
+	// Начальный и конечный пункты маршрута.
+	string _start_point, _end_point;
+	// Время движения по маршруту.
+	string _departure_time;
 
 public:
 
 	// КОНСТРУКТОРЫ.
-	marsh() : _start_point(""), _end_point(""), _route_number(0) {}
-	marsh(const string&, const string&, const int);
+	marsh() : _route_number(0), _start_point(""), _end_point(""), _departure_time("00:00:00") {}
+	marsh(const int, const string&, const string&, const string&);
 	marsh(const marsh&);
 
 	// ДЕСТРУКТОР.
 	~marsh() {}
 
 	// ГЕТТЕРЫ.
+	int get_route_number() const { return _route_number; }
 	string get_start_point() const { return _start_point; }
 	string get_end_point() const { return _end_point; }
-	int get_route_number() const { return _route_number; }
+	string get_departure_time() const { return _departure_time; }
 
 	// ПЕРЕОПРЕДЕЛЕНИЕ ОПЕРАТОРОВ.
 
 	friend ostream& operator<<(ostream&, const marsh&);
 
-	//Сравнение идёт по номерам маршрутов.
+	marsh& operator=(const marsh&);
+
+	// Сравнение идёт по номерам маршрутов
+	// Если они равны - то по времени движения поезда.
 	int operator<(const marsh&) const;
 	int operator>(const marsh&) const;
 	int operator==(const marsh&) const;
-
-	marsh& operator=(const marsh&);
 
 };
 
